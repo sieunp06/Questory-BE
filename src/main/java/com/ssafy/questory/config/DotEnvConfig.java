366980178b96694a550525d386d8c2c1aacf2550
sieunp06 <psieun0606@gmail.com>
2026-02-14 22:53:22 +0900

    feat: dotenv 환경설정 추가 및 datasource 환경 변수 설정

---
+++ src/main/java/com/ssafy/questory/config/DotEnvConfig.java
@@ -0,0 +1,17 @@
+package com.ssafy.questory.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DotEnvConfig {
+
+    @Bean
+    public Dotenv dotenv() {
+        return Dotenv.configure()
+                .directory("./")
+                .ignoreIfMissing()
+                .load();
+    }
+}
