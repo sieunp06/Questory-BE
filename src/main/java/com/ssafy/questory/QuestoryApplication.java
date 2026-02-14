e3df0750aad9d88fb38502c9f2bc3290538cfe74
sieunp06 <psieun0606@gmail.com>
2026-02-14 18:06:36 +0900

    initial commit

---
+++ src/main/java/com/ssafy/questory/QuestoryApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.questory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/questory/QuestoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.questory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
