13cabb9090ae27fad4a838e30f24de627b0a4432
sieunp06 <psieun0606@gmail.com>
2026-02-19 23:11:00 +0900

    feat: ApiResponse 추가 및 GlobalExceptionHandler 수정

--- src/main/java/com/ssafy/questory/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ssafy/questory/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.ssafy.questory.common.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,6 +11,7 @@ import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponseDto> handleCustomException(CustomException e) {
         ErrorCode code = e.getErrorCode();
@@ -24,14 +27,32 @@ public class GlobalExceptionHandler {
         List<ErrorResponseDto.FieldErrorDto> errors = e.getBindingResult()
                 .getFieldErrors()
                 .stream()
-                .map(err -> new ErrorResponseDto.FieldErrorDto(
-                        err.getField(),
-                        err.getDefaultMessage()
-                ))
+                .map(this::toFieldErrorDto)
                 .toList();
 
         return ResponseEntity
                 .status(code.getStatus())
                 .body(ErrorResponseDto.of(code, errors));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponseDto> handleHttpMessageNotReadable(HttpMessageNotReadableException e) {
+        ErrorCode code = ErrorCode.INVALID_REQUEST;
+        return ResponseEntity
+                .status(code.getStatus())
+                .body(ErrorResponseDto.of(code));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleUnexpected(Exception e) {
+        ErrorCode code = ErrorCode.INTERNAL_SERVER_ERROR;
+        return ResponseEntity
+                .status(code.getStatus())
+                .body(ErrorResponseDto.of(code));
+    }
+
+    private ErrorResponseDto.FieldErrorDto toFieldErrorDto(FieldError err) {
+        String message = (err.getDefaultMessage() != null) ? err.getDefaultMessage() : "잘못된 요청입니다.";
+        return new ErrorResponseDto.FieldErrorDto(err.getField(), message);
     }
 }
