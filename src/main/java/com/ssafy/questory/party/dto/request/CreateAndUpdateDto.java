d4bdc14b59fe9ee131fe85427b23fa82d062f9ab
sieunp06 <psieun0606@gmail.com>
2026-03-04 03:42:41 +0900

    fix: @Pattern 조건 완화

--- src/main/java/com/ssafy/questory/party/dto/request/CreateAndUpdateDto.java
+++ src/main/java/com/ssafy/questory/party/dto/request/CreateAndUpdateDto.java
@@ -1,15 +1,10 @@
 package com.ssafy.questory.party.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record CreateAndUpdateDto(
         @NotBlank(message = "파티 이름은 필수입니다.")
         @Size(max = 100, message = "파티 이름은 100자 이하여야 합니다.")
-        @Pattern(
-                regexp = "^[가-힣a-zA-Z0-9\\s]+$",
-                message = "파티 이름에는 특수문자를 사용할 수 없습니다."
-        )
         String name
 ) {}
